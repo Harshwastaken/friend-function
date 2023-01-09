b140776f0ece57c91a5ea13544c67e2994f1e2d3
Harshwastaken <119901369+Harshwastaken@users.noreply.github.com>
2023-01-09 22:01:08 +0530

    Update friend functioon.cpp

--- friend functioon.cpp
+++ friend functioon.cpp
@@ -12,24 +12,27 @@ public:
         real = a;
         img = b;
     }
-void add(complex m, complex n)
-{
-    int x,y;
-    x = m.real + n.real;
-    y = m.img + n.img;
-    cout<<"sum of complex number is  "<<endl<<x<<"+i"<<y<<endl;
-}
+    friend complex add(complex ,complex );
 };
+complex add(complex m , complex n)
+{
+    complex x;
+    x.real = m.real + n.real;
+    x.img = m.img + n.img;
+    return x;
+}
+
 
 int main()
 {
-    int p,q,m,n;
-    complex m,n;
+    int p,q;
+    complex y,z;
     cout<<"enter 1st complex number: ";
     cin>>p>>q;
-    m.get(p,q);
+    y.get(p,q);
     cout<<"enter 2nd complex number: ";
     cin>>p>>q;
-    n.get(p,q);
-    add(m,n)
+    z.get(p,q);
+    complex sum = add(y,z);
+    cout<<"sum of complex number is  "<<endl<<sum.real<<"+i"<<sum.img;
 }
